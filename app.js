9c35fc79dc170f3b08b711aaf36bf3ecd2af44b3
Sarah Hershenhouse <sarahhershey99@gmail.com>
2023-11-30 19:32:34 -0700

    Operators-exercise

--- app.js
+++ app.js
@@ -4,16 +4,46 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+console.log(`
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
 
-// Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+let sum = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+`)
 
-// YOUR CODE HERE
+console.log(`
 
-// Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
-// YOUR CODE HERE
+num + str = 1111
+num + str2 = 11eleven
+num + isPresent = 12 
+firstName + num = Frodo11
+isPresent + str = true11
+firstName + lastName = FrodoBaggins
+`)
+
+console.log(`
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+val ==str3 = true
+val === str3 = false
+!isAwake = true
+("eleven" == str4 && val >= str3) = false
+0 == false = true
+0 === false = false 
+0 != false = false
+0 !== false = true
+`)
